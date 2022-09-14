555042054442ebd4b1c574e0b1315410468d4715
Kenji Rikitake <kenji.rikitake@acm.org>
2022-09-14 16:35:39 +0900

    macos-cmake.sh: remove unnecessary flags

--- macos-cmake.sh
+++ macos-cmake.sh
@@ -3,8 +3,6 @@ rm -rf ./build ./CMakeCache.txt ./CMakeFiles
 (cd src && rm -rf ./build ./CMakeCache.txt ./CMakeFiles)
 #
 arch="x86_64"
-buildflags="-arch ${arch}"
-linkflags="-arch ${arch}"
 #
 cmake  \
     -DTQSLLIB_STATIC=ON \
@@ -13,8 +11,5 @@ cmake  \
     -DCMAKE_OSX_ARCHITECTURES="${arch}" \
     -DBDB_PREFIX=/usr/local/opt/berkeley-db \
     -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 \
-    -DCMAKE_CXX_FLAGS="${buildflags}" \
-    -DCMAKE_C_FLAGS="${buildflags}" \
-    -DCMAKE_EXE_LINKER_FLAGS=${linkerflags} \
     -DOSX_ARCH="${arch}" \
     .
